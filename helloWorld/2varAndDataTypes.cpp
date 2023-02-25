6fc19fd436233f827cd5084c9f9555a0b36c3dcb
EladAvrahami <86184072+EladAvrahami@users.noreply.github.com>
2023-02-26 00:59:32 +0200

    init

---
+++ helloWorld/2varAndDataTypes.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+
+/*int main() {
+
+	int myint;
+	myint = 15;
+	double mydouble = 3.251;
+
+	cout << myint << endl;
+	cout << mydouble << endl;
+	return 0;
+}
+*/
---
+++ helloWorld/3varAndDataTypes2.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include <string> //liboray (use string class as a data type inside main)
+using namespace std;
+/*
+int main() {
+
+	char singlechar = 'a';
+	string myname = "elad";
+	cout << singlechar << endl;
+	cout << myname << endl;
+	cout << "hello "+myname << endl;
+	cout << "hello2 " <<myname << endl;//print also like that
+
+	return 0;
+}
+*/
