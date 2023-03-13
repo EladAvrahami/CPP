6b673e7da401c2ab1db3180068bf6f0fd4e61191
EladAvrahami <86184072+EladAvrahami@users.noreply.github.com>
2023-03-13 11:14:23 +0200

    section 4 arrays

---
+++ helloWorld/33builtInArray.cpp
@@ -0,0 +1,56 @@
+#include <iostream>//liborary (contain cout&endl opptions)
+#include <string>
+using namespace std;//directive as default
+/*
+
+int main() {
+// namespace  variable    "string literal constant"
+//	std::      cout          << "hello Wrold " <<           std::endl;
+	
+	////ORDINARY ARRAY EXAMPLE 1
+	//const int ARRAY_SIZE = 5;
+	//int myArray[ARRAY_SIZE];
+
+	//myArray[0] = 15;
+	//myArray[1] = 20;
+	//myArray[2] = 3;
+	//myArray[3] = 12;
+	//myArray[4] = 10;
+
+	//for (int i = 0; i < ARRAY_SIZE; i++) {
+	//	cout << myArray[i] << endl;
+	//}
+
+
+	string names[4]{ "bob","Sally","elad","Ed" };
+	////ORDINARY ARRAY EXAMPLE 2
+	//for (int i = 0; i < 4; i++)
+	//{
+	//	cout  << names[i] << endl;
+	//}
+
+	////ORDINARY ARRAY EXAMPLE 3
+	//for (string name : names) {
+	//	cout << name << endl;
+	//}
+
+	//// ARRAY EXAMPLE 4 /using the c++ keyword auto to figured out itself what data type
+	//for (auto name : names) {
+	//	cout << name << endl;
+	//}
+
+	////Exercies:int array and print its number 
+	//const int ARRAY2SIZE = 10;
+	//int array2[ARRAY2SIZE];
+	//for (int i = 0; i < ARRAY2SIZE; i++) {
+	//	array2[i] = i+1;
+	//}
+	//cout <<  "" << endl;
+	//for (int item : array2) {//iterate the array values
+	//	cout << item << endl;
+	//}
+	
+
+	return 0;
+}
+*/
---
+++ helloWorld/34ArrayClass.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<array>
+using namespace std;
+/*
+int main() {
+	////declar array without initialization
+	//array<int, 5> myIntArray;//creat array obj new syntax
+	//for (int item : myIntArray) {
+	//	cout << item << endl;
+	//}
+
+	//// init array with params manually
+	//array<int, 5> myIntArrayInit{ 0,3,0,0,1 };
+	//for (int item : myIntArrayInit) {
+	//	cout << item << endl;
+	//}
+	//cout << "" << endl;
+
+
+	//// initlize array short syntax using just {} make all cells vals to 0   
+	//array<int, 5> myIntArrayInitShortsyntax{};
+	//for (int item : myIntArrayInitShortsyntax) {
+	//	cout << item << endl;
+	//}
+
+
+	//array<int, 4> newarray{};
+	//newarray[0] = 3;
+	//newarray[1] = 13;
+	//newarray[2] = 10;
+	//newarray[3] = 5;
+
+	//for (int item : newarray) {
+	//	cout << item << endl;
+	//}
+	//cout << "the newarray size: " << newarray.size() << endl;
+
+	//array<int, 10> missionArray{};
+	//for (int i = 0; i < missionArray.size(); i++) {
+	//	missionArray[i] = i * 2;
+	//	cout << missionArray[i] << endl;
+	//}
+	//cout << "and its size is : "  << missionArray.size() << endl;
+
+
+
+	return 0;
+}*/
